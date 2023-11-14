04a85dbb947e4f0bc19050e8914232b9c47babce
saligaros <iasonasgram85@gmail.com>
2023-11-14 10:34:25 +0200

    week06 day01 exercises and progress

---
+++ user/week06/exercises/day01/utility_classes/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {}, 
+    
+    extend: {
+      colors: {
+      'bermuda': "#ff000",
+      },
+  },
+  plugins: [],
+}
+
+
